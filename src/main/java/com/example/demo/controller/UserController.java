9e1a5f2e711921e8d2c5a2354a0fb684e64f2507
101nam101 <dophuongnam1010@gmail.com>
2025-10-26 10:46:24 +0700

    49

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,15 +5,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.service.UserService;
 // import com.example.demo.service.UserService;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
-    @RequestMapping("/")
 
-    public String getHomePage() {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("index", test);
+        model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
 }
