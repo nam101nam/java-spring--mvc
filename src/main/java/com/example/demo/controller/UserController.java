b81cd6e5d97b8eccbedbb25a633bfcac369bc3d7
101nam101 <dophuongnam1010@gmail.com>
2025-10-29 20:42:37 +0700

    53

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,11 +2,15 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
 // import com.example.demo.service.UserService;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -28,12 +32,19 @@ public class UserController {
     }
 
     @RequestMapping("/admin/user")
-    public String getAdmin(Model model) {
+    public String getUserPage(Model model) {
         String test = this.userService.handleHello();
-        model.addAttribute("index", test);
+        model.addAttribute("newUser", new User());
         model.addAttribute("hoidanit", "from controller with model");
-        model.addAttribute("create", test);
+
         return "/admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User user) {
+        System.out.println("Run here");
+        System.out.println(user);
+        return "hello";
     }
 }
 // @RestController
